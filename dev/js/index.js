d74b28410c494b92821e8b65f9c0923d81171ded
Luis Pagliaro <lpagliaro@Luiss-MacBook-Pro.local>
2017-03-06 11:42:41 -0300

    Agregadas noticias y reseñas

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,47 +1,55 @@
-function initMap() {
-  var mapDiv = document.getElementById('map'),
-    map = new google.maps.Map(mapDiv, {
-      center: {
-        // lat: -32.944709,
-        // lng: -60.655533
-        lat: -33.696774,
-        lng: -61.614476
-      },
-      zoom: 12
-    });
+(function () {
+  function initMap() {
+    var mapDiv = document.getElementById('map') || '';
 
-  var contentString = '<h4>Pandemia De Metal</h4><address>Maipú y las Vias, Elortondo, Santa Fe</address>',
-    location = {
-      lat: -33.696774,
-      lng: -61.614476
-    },
-    infoWindow = new google.maps.InfoWindow({
-      content: contentString
-    }),
-    marker = new google.maps.Marker({
-      position: location,
-      map: map,
-      title: 'Pandemia De Metal'
-    });
+    if (mapDiv !== '') {
+      var map = new google.maps.Map(mapDiv, {
+        center: {
+          // lat: -32.944709,
+          // lng: -60.655533
+          lat: -34.619530,
+          lng: -58.441316
+        },
+        zoom: 12
+      });
 
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  });
+      var contentString = '<h4>La Colorada Music Bar</h4><address>Honorio Pueyrredon 41, Caballito, Buenos Aires</address>',
+        location = {
+          lat: -34.619530,
+          lng: -58.441316
+        },
+        infoWindow = new google.maps.InfoWindow({
+          content: contentString
+        }),
+        marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: 'La Colorada Music Bar'
+        });
 
-  var reviews = document.querySelectorAll('.mainReviews article'),
-    count = 1,
-    length = reviews.length;
-
-  setInterval(function() {
-    reviews[count].style.display = 'block';
-    count === 0 ? reviews[length - 1].style.display = 'none' : reviews[count - 1].style.display = 'none';
-
-    count++;
-
-    if (count >= length) {
-      count = 0;
+      marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+      });
     }
-  }, 6000);
-}
+  }
 
-initMap();
+  function initReviews() {
+    var reviews = document.querySelectorAll('.mainReviews article'),
+      count = 1,
+      length = reviews.length;
+
+    setInterval(function () {
+      reviews[count].style.display = 'block';
+      count === 0 ? reviews[length - 1].style.display = 'none' : reviews[count - 1].style.display = 'none';
+
+      count++;
+
+      if (count >= length) {
+        count = 0;
+      }
+    }, 6000);
+  }
+
+  initMap();
+  initReviews();
+})();
